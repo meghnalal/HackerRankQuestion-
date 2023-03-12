4b4c70abd7c8992b808c106d3d875cdd74318121
meghnalal <lalmaggy@gmail.com>
2023-03-12 22:31:56 +0000

    simple_array_sum

---
+++ README.md
@@ -0,0 +1,1 @@
+Question Simple Array Sum - Solution 
---
+++ Simple Array Sum.py
@@ -0,0 +1,17 @@
+# problem : https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    x=sum(ar)
+    return x
